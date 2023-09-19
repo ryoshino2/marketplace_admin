8106620da7c18b51598c695d57aa9c58fd634909
ryoshino2 <rafayoshino02@gmail.com>
2023-09-19 19:23:26 -0300

    fix unit test ProductServiceTest

--- src/test/java/marketplace/demo/service/ProductServiceTest.java
+++ src/test/java/marketplace/demo/service/ProductServiceTest.java
@@ -2,6 +2,7 @@ package marketplace.demo.service;
 
 import marketplace.demo.model.Product;
 import marketplace.demo.model.ProductBuilder;
+import marketplace.demo.model.response.ProductDto;
 import marketplace.demo.repository.ProductRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,11 +10,13 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
@@ -25,23 +28,32 @@ public class ProductServiceTest {
     @MockBean
     private ProductRepository productRepository;
 
+    @MockBean
+    private ModelMapper mockModelMapper;
+
     @InjectMocks
     private ProductServiceImpl productService;
 
     public Product product = null;
     public List<Product> productList;
+    public ProductDto productDto = new ProductDto();
 
     @BeforeEach
     public void setUp() {
         product = ProductBuilder.with().name("hello").price(14.7).withDescription("DELL").build();
         productList = new ArrayList<>();
         productList.add(product);
+        productDto.setDescription(product.getDescription());
+        productDto.setName(product.getName());
+        productDto.setPrice(product.getPrice());
+
     }
 
     @Test
     public void whenAddProductAllParamValidShouldReturnSuccess() {
         doReturn(product).when(productRepository).save(any());
         doReturn(productList).when(productRepository).findAll();
+        doReturn(productDto).when(mockModelMapper).map(product, ProductDto.class);
 
         productService.registerProduct(product);
 
